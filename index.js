a72cd70bb7bc50999856405714bd07e5f901e0c5
William Bugenis <wbugenis@gmail.com>
2021-02-28 15:22:26 -0500

    commit tree 8693a0d02c65846fabc5a27a88313641e2bb3d3b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// const { FetchError } = require("node-fetch")
+
+const db = 'http://localhost:3000'
+
+// const body = document.querySelector("body")
+
+function submitData(name, email){
+    return fetch(db + '/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(response => response.json())
+    .then(newUser => {
+        document.body.innerHTML = newUser["id"]
+    })
+    .catch(error => document.body.innerHTML = error.message)
+}
+// submitData("john", "email@email.com")
